a198fcd96f51d44a26de8c573b6a4ada648668ec
Michael Zhao <michaelzhao314@gmail.com>
2021-05-12 23:44:32 -0500

    Added about section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,101 +1,62 @@
 <template>
     <div class="home">
-        <img class="background" src="../assets/background.jpg" />
-        <div class="name-box">
-            <h1 id="name">Michael Zhao</h1>
-            <p id="tagline">programmer · designer · artist</p>
+        <div class="home-top">
+            <div class="navbar">
+                <a class="nav-link" href="#about-me">about me</a>
+                <a class="nav-link" href="#recent-posts">recent posts</a>
+                <a class="nav-link" href="#my-technology">my tech</a>
+                <a class="nav-link" href="#cool-stuff">cool stuff</a>
+                <a class="nav-link" href="/blog">blog</a>
+            </div>
+            <img class="background" src="../assets/background.jpg" />
+            <div class="name-box">
+                <h1 id="name">Michael Zhao</h1>
+                <p id="tagline">programmer · designer · artist</p>
+            </div>
+            <div class="background-fade"></div>
         </div>
-        <div class="background-fade"></div>
-        <GithubLogo id="github-logo" class="logo" />
+        <div class="bottom-content">
+            <h2 id="about-me">About Me</h2>
+            <div class="about-container">
+                <div class="about-left">
+                    <p class="description">
+                        Hi! I’m a full-time student and code for fun :)) I mostly work on frontend
+                        UI/UX design projects, as well as full-stack applications and services. I
+                        also love drawing and exploring new mediums, as well as cooking whenever I
+                        get the opportuntity to. Feel free to shoot me an email or message, and I’ll
+                        be happy to talk to you!
+                    </p>
+                    <p class="email">
+                        Email:
+                        <a class="email-a" href="mailto:me@michaelzhao.xyz">me@michaelzhao.xyz</a>
+                    </p>
+                    <div class="link-logo-container">
+                        <GithubLogo id="github-logo" class="logo" />
+                        <LinkedInLogo id="linkedin-logo" class="logo" />
+                    </div>
+                </div>
+                <img class="about-img" v-bind:src="aboutImg" alt="My picture!" />
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import GithubLogo from '../assets/logos/github.vue';
-export default { name: 'Home', components: { GithubLogo } };
+import LinkedInLogo from '../assets/logos/linkedin.vue';
+import aboutImg from '../assets/face.png';
+
+export default {
+    name: 'Home',
+    components: { GithubLogo, LinkedInLogo },
+    data() {
+        return {
+            aboutImg,
+        };
+    },
+};
 </script>
 
 <style scoped>
-h1 {
-    font-family: 'Montserrat';
-    font-weight: 300;
-    font-size: 6rem;
-    color: white;
-}
-
-h2 {
-    font-family: 'Ubuntu';
-    font-weight: 700;
-    font-size: 4rem;
-    background-clip: text;
-}
-
-p {
-    font-family: 'Open Sans';
-    font-weight: 600;
-    font-size: 1rem;
-    color: #888;
-}
-
-.home {
-    width: 100%;
-    height: 100%;
-    position: relative;
-}
-
-.background {
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: -10;
-
-    width: 100%;
-    height: 100vh;
-    object-fit: cover;
-    object-position: center top;
-}
-
-.background-fade {
-    height: 25vh;
-    width: 100vw;
-    position: absolute;
-    top: 75vh;
-    left: 0;
-
-    background: linear-gradient(180deg, #21212100, #212121);
-}
-
-.name-box {
-    width: 65vw;
-    height: 40vh;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    z-index: -9;
-    margin: -20vh 0 0 -32.5vw;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    background-color: #00000088;
-}
-
-#name {
-    margin: 0.5rem 0;
-}
-
-#tagline {
-    margin: 0;
-    font-family: 'Montserrat';
-    font-weight: 300;
-    font-size: 2rem;
-    color: #9bf3ff;
-}
-
-.logo {
-    display: none;
-    /* Temporary lol */
-}
+@import './Home.css';
 </style>
