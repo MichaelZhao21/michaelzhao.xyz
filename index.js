c3803af8635b29d8184c6d42d1b956173a47cd46
Michael Zhao <michaelzhao314@gmail.com>
2021-09-07 14:26:38 -0500

    Created color changing title and updated lists

---
+++ index.js
@@ -0,0 +1,13 @@
+let hsl = 191;
+let hsl2 = 263;
+
+$(() => {
+    setInterval(() => {
+        hsl++;
+        hsl2++;
+        if (hsl > 360) hsl -= 360;
+        if (hsl2 > 360) hsl2 -= 360;
+        $('h1').css('--primary', `hsl(${hsl}, 100%, 61%)`);
+        $('h1').css('--secondary', `hsl(${hsl2}, 100%, 61%)`);
+    }, 200);
+});
