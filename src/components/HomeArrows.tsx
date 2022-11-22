d03b93148fe671b0c3cce7766cabd4082c0d7c77
Michael Zhao <michaelzhao314@gmail.com>
2022-11-22 10:42:51 -0600

    Added arrow move down functionality

--- src/components/HomeArrows.tsx
+++ src/components/HomeArrows.tsx
@@ -2,9 +2,17 @@ import Image from 'next/image';
 import styles from '../styles/HomeArrows.module.css';
 
 const HomeArrows = () => {
+    const scrollDown = () => {
+        window.scrollBy({
+            top: window.innerHeight,
+            left: 0,
+            behavior: 'smooth',
+        });
+    };
+
     return (
         <div className={styles.container}>
-            <div className={styles.arrow}>
+            <div className={styles.arrow} onClick={scrollDown}>
                 <Image src="/arrows.svg" layout="fill" objectFit="contain" />
             </div>
         </div>
