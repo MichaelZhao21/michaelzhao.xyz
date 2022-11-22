4ab0f7f16a50c4bb008a972a064f23e58afd868e
Michael Zhao <michaelzhao314@gmail.com>
2022-11-21 23:42:27 -0600

    Added tagline

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,14 @@ import type { NextPage } from 'next';
 import styles from '../src/styles/Home.module.css';
 import HomeTitle from '../src/components/HomeTitle';
 import HomeArrows from '../src/components/HomeArrows';
+import HomeTagline from '../src/components/HomeTagline';
 
 const Home: NextPage = () => {
     return (
         <div className={styles.container}>
             <HomeTitle />
             <HomeArrows />
+            <HomeTagline />
         </div>
     );
 };
---
+++ src/components/HomeTagline.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import styles from '../styles/HomeTagline.module.css';
+import data from '../data.json';
+import Image from 'next/image';
+
+const HomeTagline = () => {
+    const [quote, setQuote] = useState('');
+    // Get a random tagline
+    useEffect(() => {
+        const quoteNum = Math.floor(Math.random() * data.quotes.length);
+        setQuote(data.quotes[quoteNum]);
+    }, []);
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.quoteContainer}>
+                <Image src="/quote.svg" layout="fill" objectFit="contain" />
+            </div>
+            <p className={styles.quote}>{quote}</p>
+        </div>
+    );
+};
+
+export default HomeTagline;
