5e5bdbdb21ba6920ecc9ad7f7d7d9a88e685d429
Michael Zhao <michaelzhao314@gmail.com>
2023-01-04 16:34:11 -0600

    Created new layout for projects display

--- src/components/HomeProjects.tsx
+++ src/components/HomeProjects.tsx
@@ -1,82 +1,53 @@
 import styles from '../styles/HomeProjects.module.css';
 import data from '../data.json';
-import { useEffect, useState } from 'react';
-import Image from 'next/image';
-import Circle from './Circle';
 
 const HomeProjects = () => {
-    const [matches, setMatches] = useState(false);
-    const [projectId, setProjectId] = useState(0);
-    const [timer, setTimer] = useState<NodeJS.Timeout>();
-
-    const switchProject = (id: number) => {
-        setProjectId(id);
-    };
-
-    useEffect(() => {
-        clearTimeout(timer);
-        setTimer(
-            setTimeout(() => {
-                setProjectId((projectId + 1) % data.projects.length);
-            }, 5000)
-        );
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [projectId]);
-
-    useEffect(() => {
-        setMatches(window.matchMedia('(max-width: 900px)').matches);
-        window
-            .matchMedia('(max-width: 900px)')
-            .addEventListener('change', (e) => setMatches(e.matches));
-    }, []);
-
     return (
-        <div className={styles.container}>
-            <div className={styles.subContainer}>
-                <div className={styles.displayBox}>
-                    <a
-                        href={data.projects[projectId].href}
-                        target="_blank"
-                        rel="noreferrer"
-                        className={styles.displayBox}
-                        style={{ borderColor: data.projects[projectId].color }}
-                    >
-                        <Image
-                            src={`/${data.projects[projectId].name}.jpg`}
-                            alt={data.projects[projectId].name}
-                            layout="fill"
-                            objectFit="cover"
-                            style={{ borderRadius: '0.5rem' }}
+        <div>
+            <div className={styles.mobileProjects + ' ' + styles.padded}>
+                {data.projects.map((p) => (
+                    <div className={styles.mobileProject}>
+                        <img
+                            src={`/${p.name}.jpg`}
+                            alt={p.name}
+                            style={{ borderColor: p.color }}
                         />
-                    </a>
-                </div>
-                <div className={styles.column}>
-                    {data.projects.map((p, i) => (
-                        <div
-                            key={p.name}
-                            className={
-                                styles.projectContainer +
-                                (i == projectId
-                                    ? ` ${styles.activeProject}`
-                                    : '')
-                            }
-                            onClick={switchProject.bind(this, i)}
-                        >
-                            <Circle
-                                size={matches ? "1rem" : "2.5rem"}
-                                fill={p.color as string}
-                                noAnimation
-                                staticPosition
-                            />
-                            <div className={styles.projectName}>
-                                {p.display}
-                            </div>
+                        <div className={styles.mobileProjectTag}>
+                            <h2 style={{ color: p.color }}>{p.display}</h2>
+                            <p className={styles.projectSubtitle}>
+                                {p.position}
+                            </p>
+                            <p className={styles.projectDesc}>
+                                {p.description}
+                            </p>
                         </div>
-                    ))}
-                </div>
+                    </div>
+                ))}
             </div>
-            <div className={styles.description}>
-                {data.projects[projectId].description}
+            <div className={styles.container}>
+                {data.projects.map((p) => (
+                    <div
+                        key={p.name}
+                        className={styles.project}
+                        style={{
+                            backgroundImage: `url(/${p.name}.jpg)`,
+                            borderColor: p.color,
+                        }}
+                    >
+                        <div
+                            className={styles.projectTag}
+                            style={{ borderColor: p.color }}
+                        >
+                            <h2 style={{ color: p.color }}>{p.display}</h2>
+                            <p className={styles.projectSubtitle}>
+                                {p.position}
+                            </p>
+                            <p className={styles.projectDesc}>
+                                {p.description}
+                            </p>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     );
