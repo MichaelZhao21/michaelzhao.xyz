91a583dfaca9d90c23747d16edb4a6856fd65db1
Michael Zhao <michaelzhao314@gmail.com>
2024-09-04 13:31:56 -0500

    Added web format for projects display

--- src/routes/projects/project.svelte
+++ src/routes/projects/project.svelte
@@ -5,6 +5,7 @@
 	export let roles: string;
 	export let description: string;
 	export let color: string;
+	export let url: string;
 </script>
 
 <div class="project">
@@ -12,13 +13,19 @@
 		<img src="/imgs/circle.svg" alt="o" class="circle" />
 		<p class="date">{date}</p>
 	</div>
+	<div class="web-date-cont">
+		<p class="web-date">{date}</p>
+		<img src="/imgs/circle.svg" alt="o" class="web-circle" />
+	</div>
 	<div class="right">
-		<img
-			src={`/imgs/${name}.jpg`}
-			alt={`project ${name} thumbnail`}
-			class="thumb"
-			style={`border-color: #${color}`}
-		/>
+		<a href={url}>
+			<img
+				src={`/imgs/${name}.jpg`}
+				alt={`project ${name} thumbnail`}
+				class="thumb"
+				style={`border-color: #${color}`}
+			/>
+		</a>
 		<div class="desc">
 			<h2 class="title" style={`color: #${color}`}>{display}</h2>
 			<h3 class="subtitle">{roles}</h3>
@@ -28,8 +35,33 @@
 </div>
 
 <style>
-	.date-cont {
+	.project {
 		display: flex;
+		flex-direction: row;
+		margin-bottom: 2rem;
+	}
+
+	.web-date-cont {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		margin-left: -23rem;
+	}
+
+	.web-date {
+		font-size: 1.5rem;
+		color: var(--text-accent-light);
+		width: 20rem;
+		text-align: right;
+	}
+
+	.web-circle {
+		height: 1.5rem;
+		margin-left: 1.5rem;
+	}
+
+	.date-cont {
+		display: none;
 		flex-direction: row;
 		align-items: center;
 	}
@@ -42,15 +74,20 @@
 
 	.right {
 		padding-top: 0.75rem;
-		padding-left: 2rem;
+		padding-left: 2.5rem;
 		display: flex;
 		flex-direction: row;
 	}
 
 	.thumb {
-		border: 6px solid white;
+		border: 5px solid white;
 		border-radius: 10px;
-		height: 18rem;
+		height: 15rem;
+		transition-duration: 200ms;
+	}
+
+	.thumb:hover {
+		transform: scale(1.05);
 	}
 
 	.circle {
@@ -65,7 +102,7 @@
 	.title {
 		font-size: 1.75rem;
 		font-family: 'Rubik', 'Roboto', 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-        font-weight: bold;
+		font-weight: bold;
 	}
 
 	.subtitle {
@@ -74,8 +111,33 @@
 		color: var(--text-accent-light);
 	}
 
+	.notes {
+		font-size: 1.15rem;
+	}
+
+	/* Anything less than 1200 (landscape tablets) */
+	@media only screen and (max-width: 1200px) {
+		.web-date-cont {
+			margin-left: -13rem;
+		}
+		.web-date {
+			width: 10rem;
+		}
+		.thumb {
+			height: 8rem;
+			border-width: 3px;
+		}
+	}
+
 	/* Anything less than 768 (phones, vertical tablets) */
 	@media only screen and (max-width: 768px) {
+		.web-date-cont {
+			display: none;
+		}
+
+		.date-cont {
+			display: flex;
+		}
 		.project {
 			flex-direction: column;
 		}
--- src/routes/projects/projects.svelte
+++ src/routes/projects/projects.svelte
@@ -16,15 +16,28 @@
 			description="The TAMS Club Calendar is a fully contained event tracker, club/volunteering database, and general resource center. This is the unofficial club event calendar for the Texas Academy of Mathematics and Science (TAMS) student organizations!"
 			roles="Creator, Developer, Designer"
 			color="00BB00"
+			url="https://tams.club"
+		/>
+		<Project
+			name="jury"
+			date="Dec 2022 - Present"
+			display="Jury (Hackathon Judging Platform)"
+			description="A project designed to create a new pairwise judging system using modern technologies aimed at optimizing the user experience of judges and admin users. Using the same underlying statistical models and paper as Gavel, Jury is trying to deliver the best possible experience to everyone at the end of a grueling hackathon."
+			roles="Creator, Developer, Designer"
+			color="00ACE6"
+			url="https://jury.mikz.dev"
 		/>
 	</div>
 </div>
+<p class="others">...and a lot more :D</p>
+<p class="others">See my Github for ALL the lil projects and unfinished code bits I've done!</p>
 
 <style>
 	.cont {
 		display: flex;
 		flex-direction: row;
-		padding-left: 1rem;
+		padding-left: 22rem;
+		margin-bottom: 2rem;
 	}
 	.line {
 		width: 3px;
@@ -47,7 +60,20 @@
 	}
 
 	.proj-list {
-		padding-top: 1rem;
+		padding: 1rem 0;
+	}
+
+	.others {
+		color: var(--text-accent);
+		text-align: center;
+		font-size: 1.5rem;
+	}
+
+	/* Anything less than 1200 (landscape tablets) */
+	@media only screen and (max-width: 1200px) {
+		.cont {
+			padding-left: 8rem;
+		}
 	}
 
 	/* Anything less than 768 (phones, vertical tablets) */
